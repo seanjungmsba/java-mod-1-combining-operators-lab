16dbe17e6815a81bded8298f02edc41cc0a9970c
seanjungmsba <seanjeong1996@gmail.com>
2022-06-22 16:21:52 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+// Truth table for && operator:
+ // true && true = true
+ // true && false = false
+ // false && false = false
+ // false && true = false
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+		boolean areYouHungry = true;
+        boolean areyouThirsty = false;
+        boolean areYouSleepy = true;
+        boolean areYouSad = false;
+        
+        System.out.println(areYouHungry && areYouSleepy);
+        System.out.println(areYouHungry && areyouThirsty);
+        System.out.println(areyouThirsty && areYouSad);
+        System.out.println(areyouThirsty && areYouSleepy);
+
     }
 }
