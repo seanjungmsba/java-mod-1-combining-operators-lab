e8e3439353771867301b8136eb8ee26b3ef4950a
seanjungmsba <seanjeong1996@gmail.com>
2022-06-22 16:25:21 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@
 public class Main {
     public static void main(String[] args) {
         // your code here
+        
 		boolean areYouHungry = true;
         boolean areyouThirsty = false;
         boolean areYouSleepy = true;
