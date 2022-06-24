39c318e99effc4e8c8295ebb75b475860f1e4c52
seanjungmsba <seanjeong1996@gmail.com>
2022-06-24 14:41:57 -0400

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,21 @@
-// Truth table for && operator
- // true && true = true
- // true && false = false
- // false && false = false
- // false && true = false
-
 public class Main {
     public static void main(String[] args) {
+        
         // your code here
+        boolean[][] truthMatrix = { {true,true},{true,false},
+                                    {false,false},{false,true} };
         
-		boolean isTrue = true;
-        boolean isFalse = false;
-        
-        System.out.println("Truth Table for &&: ");
-        System.out.println(isTrue && isTrue); // returns true
-        System.out.println(isTrue && isFalse); // returns false
-        System.out.println(isFalse && isFalse); // returns false
-        System.out.println(isFalse && isTrue); // returns false
+        System.out.println("Truth table for && operator: ");
+        for (boolean[] b : truthMatrix) {
+            System.out.println(b[0] + " && " + b[1] + " = " + (b[0] && b[1]));
+        }
 
-        System.out.println("===================");
+        System.out.println("============================");
 
-        System.out.println("Truth Table for ||: ");
-        System.out.println(isTrue || isTrue); // returns true
-        System.out.println(isTrue || isFalse); // returns true
-        System.out.println(isFalse || isFalse); // returns false
-        System.out.println(isFalse || isTrue); // returns true
+        System.out.println("Truth table for || operator: ");
+        for (boolean[] b : truthMatrix) {
+            System.out.println(b[0] + " || " + b[1] + " = " + (b[0] || b[1]));
+        }
 
     }
 }
